d0020e30220faa0816a5a4005e269138d21ecf97
OguzKagankrks <okagankrks@gmail.com>
2025-08-30 23:44:50 +0300

    feat(Home IT2): redesign home page per visual (hero banner, categories, promos, popular chips, product cards) while keeping functional CTA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,43 +5,85 @@ function Home() {
   return (
     <main className='home'>
       <div className='container'>
-      <header className='hero'>
-        <div className='hero__content'>
-          <h1 className='brand'>Teknolojik Yemekler</h1>
-          <p className='tagline'>Ac misin? En hizlisi burada.</p>
-          <Link className='cta' to='/form'>Siparis Olustur</Link>
-        </div>
-        <div className='hero__image' aria-hidden>
-          <img src='/images/iteration-2-images/pictures/food-1.png' alt='Lezzetli pizza' />
-        </div>
-      </header>
+        {/* HERO BANNER */}
+        <section className='hero-banner'>
+          <div className='hero-inner'>
+            <h1 className='site-brand'>Teknolojik Yemekler</h1>
+            <p className='hero-sub'>firsati kacirma</p>
+            <h2 className='hero-title'>KOD ACIKTIRIR<br/>PIZZA, DOYURUR</h2>
+            <Link className='btn-primary' to='/form'>ACIKTIM</Link>
+            <img className='hero-pizza' src='/images/iteration-2-images/pictures/food-1.png' alt='Pizza' />
+          </div>
+          <div className='categories-bar'>
+            <ul>
+              <li><img src='/images/iteration-2-images/icons/1.svg' alt=''/> YENI! Kore</li>
+              <li><img src='/images/iteration-2-images/icons/2.svg' alt=''/> Pizza</li>
+              <li><img src='/images/iteration-2-images/icons/3.svg' alt=''/> Burger</li>
+              <li><img src='/images/iteration-2-images/icons/4.svg' alt=''/> Kizartmalar</li>
+              <li><img src='/images/iteration-2-images/icons/5.svg' alt=''/> Fast food</li>
+              <li><img src='/images/iteration-2-images/icons/6.svg' alt=''/> Gazi icecek</li>
+            </ul>
+          </div>
+        </section>
 
-      <section className='features'>
-        <article className='card'>
-          <img src='/images/iteration-2-images/cta/kart-1.png' alt='Hizli teslimat' />
-          <h3>Hizli Teslimat</h3>
-          <p>Siparislerin rekor surede kapinda.</p>
-        </article>
-        <article className='card'>
-          <img src='/images/iteration-2-images/cta/kart-2.png' alt='Taptaze malzemeler' />
-          <h3>Taptaze</h3>
-          <p>Her gun taze malzemelerle hazirlanir.</p>
-        </article>
-        <article className='card'>
-          <img src='/images/iteration-2-images/cta/kart-3.png' alt='Zengin secenek' />
-          <h3>Bol Secenek</h3>
-          <p>Her damak tadina uygun kombinler.</p>
-        </article>
-      </section>
+        {/* PROMOS */}
+        <section className='promos'>
+          <article className='promo promo-large'>
+            <div className='promo-content'>
+              <h3>Ozel Lezzetus</h3>
+              <p>Position Absolute Aci Burger</p>
+              <Link to='/form' className='btn-ghost'>siparis ver</Link>
+            </div>
+            <img src='/images/iteration-2-images/pictures/food-2.png' alt='' />
+          </article>
+          <article className='promo promo-dark'>
+            <div className='promo-content'>
+              <h4>Hackathlon Burger Menu</h4>
+              <Link to='/form' className='btn-ghost'>siparis ver</Link>
+            </div>
+            <img src='/images/iteration-2-images/cta/kart-3.png' alt='' />
+          </article>
+          <article className='promo promo-warm'>
+            <div className='promo-content'>
+              <h4>Çoooook hizli npm gibi kurye</h4>
+              <Link to='/form' className='btn-ghost'>siparis ver</Link>
+            </div>
+            <img src='/images/iteration-2-images/cta/kart-2.png' alt='' />
+          </article>
+        </section>
 
-      <section className='gallery'>
-        <img src='/images/iteration-2-images/footer/insta/li-0.png' alt='Galeri 1' />
-        <img src='/images/iteration-2-images/footer/insta/li-1.png' alt='Galeri 2' />
-        <img src='/images/iteration-2-images/footer/insta/li-2.png' alt='Galeri 3' />
-        <img src='/images/iteration-2-images/footer/insta/li-3.png' alt='Galeri 4' />
-        <img src='/images/iteration-2-images/footer/insta/li-4.png' alt='Galeri 5' />
-        <img src='/images/iteration-2-images/footer/insta/li-5.png' alt='Galeri 6' />
-      </section>
+        {/* POPULAR */}
+        <section className='popular'>
+          <p className='section-kicker'>en cok paketlenen menuler</p>
+          <h3 className='section-title'>Aciktiran Kodlara Doyuran Lezzetler</h3>
+
+          <div className='chips'>
+            <span className='chip'>Ramen</span>
+            <span className='chip active'>Pizza</span>
+            <span className='chip'>Burger</span>
+            <span className='chip'>French fries</span>
+            <span className='chip'>Fast food</span>
+            <span className='chip'>Soft drinks</span>
+          </div>
+
+          <div className='products'>
+            <article className='product'>
+              <img src='/images/iteration-2-images/pictures/food-1.png' alt='Terminal Pizza' />
+              <h4>Terminal Pizza</h4>
+              <div className='meta'><span>4.9</span><span>(200)</span><strong>60₺</strong></div>
+            </article>
+            <article className='product'>
+              <img src='/images/iteration-2-images/pictures/food-2.png' alt='Position Absolute Aci Pizza' />
+              <h4>Position Absolute Aci Pizza</h4>
+              <div className='meta'><span>4.9</span><span>(829)</span><strong>85₺</strong></div>
+            </article>
+            <article className='product'>
+              <img src='/images/iteration-2-images/pictures/food-3.png' alt='useEffect Tavuklu Burger' />
+              <h4>useEffect Tavuklu Burger</h4>
+              <div className='meta'><span>4.9</span><span>(462)</span><strong>75₺</strong></div>
+            </article>
+          </div>
+        </section>
       </div>
     </main>
   )
